a9d607d4527394e3a7802f8a40a8813fb8cf9ac3
Kolja Dummann <k.dummann@gmail.com>
2011-06-18 20:15:09 +0200

    ril: fix signal bars

--- galaxys2.mk
+++ galaxys2.mk
@@ -106,6 +106,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
        wifi.interface=eth0 \
        wifi.supplicant_scan_interval=20 \
        ro.telephony.ril_class=samsung \
+       ro.telephony.sends_barcount=1 \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        dalvik.vm.heapsize=32m \
        persist.service.usb.setting=0 \
