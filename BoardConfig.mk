9bd1a8fae085bbc2c52ba56854392b8eb1725053
Kolja Dummann <k.dummann@gmail.com>
2011-06-18 13:15:12 +0200

    remove prerecovery command

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,6 @@ BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/c1-common/shbootimg.mk
-TARGET_RECOVERY_PRE_COMMAND="echo 1 > /cache/.startrecovery; sync;"
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxys2,GT-I9100
