CyanogenMod/android_device_samsung_galaxys2

commit f2cda322b7375bcb3cdfc8193fc8986f86a81caa

recovery: don't allow power button to toggle display

recovery/recovery_ui.c
@@ -46,7 +46,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_BACK);
 }
 
 int device_reboot_now(volatile char* key_pressed, int key_code) {
@@ -88,7 +88,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_BACKSPACE:
             case KEY_SEARCH:
             case KEY_BACK:
-                return GO_BACK;
+                return NO_ACTION;
         }
     }
 

commit 922120460f26007c8ec4007eb71ca71034103a57

Fix back button in recovery, broken by f2cda322
Change-Id: I0e59d6096fb0ccbc2407172b48189c457496e1df

recovery/recovery_ui.c
@@ -88,7 +88,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_BACKSPACE:
             case KEY_SEARCH:
             case KEY_BACK:
-                return NO_ACTION;
+                return GO_BACK;
         }
     }
 