529dc8a02a4351e1c97996a3b9e9bebbacb58e7b
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-05-28 11:21:17 +0200

    galaxys2: use aosp charger
    Change-Id: I1b756a776a8ce43d5293a4b22def88462e7e5463

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,11 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/galaxys2/overlay/include
 
+# Charging mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGER_RES := device/samsung/galaxys2/res/charger
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxys2,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T
 
--- galaxys2_base.mk
+++ galaxys2_base.mk
@@ -80,6 +80,11 @@ PRODUCT_PACKAGES += \
     libs5pjpeg \
     libfimg
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
 # MFC API
 PRODUCT_PACKAGES += \
     libsecmfcapi
