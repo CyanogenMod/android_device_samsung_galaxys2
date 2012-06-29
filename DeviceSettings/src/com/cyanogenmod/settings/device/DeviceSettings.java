3e2e0f38f5a1b4818fd9412b8ce58d3b20ad168f
Sam Mortimer <sam@mortimer.me.uk>
2012-06-28 21:58:22 -0700

    galaxys2/i777: configurable touchkey backlight timeout (2/2)
    requires smdk4210 kernel change:
    http://review.cyanogenmod.com/#/c/18357/
    
    Change-Id: Icd4bd6749886ef1eaf7f4527bf7052a18b549868

--- DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -45,6 +45,7 @@ public class DeviceSettings extends FragmentActivity {
     public static final String KEY_CALIBRATE_GYRO = "calibrate_gyro";
     public static final String KEY_TOUCHSCREEN_SENSITIVITY = "touchscreen_sensitivity";
     public static final String KEY_TOUCHKEY_LIGHT = "touchkey_light";
+    public static final String KEY_TOUCHKEY_TIMEOUT = "touchkey_timeout";
     public static final String KEY_VIBRATOR_INTENSITY = "vibrator_intensity";
 
     ViewPager mViewPager;
